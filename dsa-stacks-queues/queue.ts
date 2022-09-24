ad4c38196f9791d12fa51f7a7b43345433d43ce7
Gerard Keiser <gerardkeiser@gmail.com>
2022-09-24 14:31:22 -0500

    Enqueue passing

--- dsa-stacks-queues/queue.ts
+++ dsa-stacks-queues/queue.ts
@@ -1,7 +1,10 @@
 /** Node: node for a queue. */
 
 class Node {
-  constructor(val) {
+  val: any;
+  next: Node | null;
+
+  constructor(val: any) {
     this.val = val;
     this.next = null;
   }
@@ -11,6 +14,10 @@ class Node {
  *  remove from the front or add to the back. */
 
 class Queue {
+  first: Node | null;
+  last: Node | null;
+  size: number;
+
   constructor() {
     this.first = null;
     this.last = null;
@@ -19,28 +26,29 @@ class Queue {
 
   /** enqueue(val): add new value to end of the queue. Returns undefined. */
 
-  enqueue(val) {
-
+  enqueue(val: any) {
+    const node = new Node(val);
+    if (this.size === 0) {
+      this.first = node;
+    } else {
+      this.last.next = node;
+    }
+    this.last = node;
+    this.size++;
   }
 
   /** dequeue(): remove the node from the start of the queue
    * and return its value. Should throw an error if the queue is empty. */
 
-  dequeue() {
-
-  }
+  dequeue() {}
 
   /** peek(): return the value of the first node in the queue. */
 
-  peek() {
-
-  }
+  peek() {}
 
   /** isEmpty(): return true if the queue is empty, otherwise false */
 
-  isEmpty() {
-
-  }
+  isEmpty() {}
 }
 
-module.exports = Queue;
+export default Queue;
