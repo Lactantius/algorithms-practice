ecdcbbbc0b7cd738ccc7e814f40d06b1268fc63f
Gerard Keiser <gerardkeiser@gmail.com>
2022-09-24 09:55:20 -0500

    Push method done

--- dsa-arrays-linked-lists/linked-list.ts
+++ dsa-arrays-linked-lists/linked-list.ts
@@ -27,7 +27,16 @@ class LinkedList {
 
   /** push(val): add new value to end of list. */
 
-  push(val) {}
+  push(val) {
+    const node = new Node(val);
+    if (!this.head) {
+      this.head = node;
+      this.tail = node;
+    }
+    this.tail.next = node;
+    this.tail = node;
+    this.length++;
+  }
 
   /** unshift(val): add new value to start of list. */
 
