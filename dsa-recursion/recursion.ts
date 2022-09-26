754ca7f3dbaa6394e59e0a4fa0ed186d638c5ce5
Gerard Keiser <gerardkeiser@gmail.com>
2022-09-26 10:16:58 -0500

    All recursion tests passing

--- dsa-recursion/recursion.ts
+++ dsa-recursion/recursion.ts
@@ -8,36 +8,71 @@ function product(nums: number[], idx = 0): number {
 
 /** longest: return the length of the longest word in an array of words. */
 
-function longest(words) {}
+function longest(words: string[]): number {
+  console.log(words);
+  if (words.length === 0) return 0;
+
+  return Math.max(words[0].length, longest(words.slice(1)));
+}
 
 /** everyOther: return a string with every other letter. */
 
-function everyOther(str) {}
+function everyOther(str: string): string {
+  if (str.length === 0) return "";
+  return str[0] + everyOther(str.slice(2));
+}
 
 /** isPalindrome: checks whether a string is a palindrome or not. */
 
-function isPalindrome(str) {}
+function isPalindrome(str: string): boolean {
+  return str === revString(str);
+}
 
 /** findIndex: return the index of val in arr (or -1 if val is not present). */
 
-function findIndex(arr, val) {}
+function findIndex(arr: any[], val: any, idx = 0): number {
+  if (idx === arr.length - 1) return -1;
+  if (arr[idx] === val) return idx;
+  return findIndex(arr, val, idx + 1);
+}
 
 /** revString: return a copy of a string, but in reverse. */
 
-function revString(str) {}
+function revString(str: string): string {
+  if (str.length === 1) return str[0];
+  return str[str.length - 1] + revString(str.slice(0, str.length - 1));
+}
 
 /** gatherStrings: given an object, return an array of all of the string values. */
 
 function gatherStrings(obj: Object): string[] {
   return Object.values(obj)
-    .map((val) => (typeof val === "object" ? gatherStrings(val) : val))
-    .filter((val) => typeof val === "string");
+    .map((val) => {
+      if (typeof val === "object") return gatherStrings(val);
+      if (typeof val === "string") return val;
+    })
+    .flat();
 }
 
 /** binarySearch: given a sorted array of numbers, and a value,
  * return the index of that value (or -1 if val is not present). */
 
-function binarySearch(arr, val) {}
+function binarySearch(arr: any[], val: any): number {
+  const left = 0;
+  const right = arr.length - 1;
+  const middle = Math.floor((left + right) / 2);
+
+  if (arr[middle] === val) return middle;
+  if (val < arr[middle]) {
+    return binarySearch(arr.slice(left, middle), val);
+  }
+  if (arr[middle] < val) {
+    const next = binarySearch(arr.slice(middle + 1, right + 1), val);
+    return next < 0 ? -1 : next + middle + 1;
+  }
+
+  return -1;
+}
 
 export {
   product,
